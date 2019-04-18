c1630a6b6ed898018b51c677db2cfca25f1a462e
Esteban Puentes <esteban.puentes@cern.ch>
2019-04-18 11:56:10 +0200

    Upload base code

---
+++ heartbeat/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace plugin
+{
+    [PluginAttribute(PluginName = "HeartBeat")]
+    public class IUpdates : IInputPlugin
+    {
+        public string Execute()
+        {
+            return "{ 1 }";
+        }
+
+    }
+}
