3834c77b1c5446b6d4a4b6cd4faedec0cb5d3733
Esteban Puentes <esteban.puentes@cern.ch>
2019-07-19 15:12:53 +0200

    Add plugin functionality

---
+++ heartbeat/IHeartbeat.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+
+using plugin;
+
+namespace heartbeat
+{
+    [PluginAttribute(PluginName = "HeartBeat")]
+    public class IHeartbeat : IInputPlugin
+    {
+        public string Execute(JObject settings)
+        {
+            var heartBeatMessage = new Models.HeartBeatMessage
+            {
+                HostName = System.Environment.MachineName,
+                OperativeSystem = System.Environment.OSVersion.VersionString,
+                DateTime = DateTime.Now
+            };
+            
+            return JsonConvert.SerializeObject(heartBeatMessage);
+        }
+
+    }
+}
