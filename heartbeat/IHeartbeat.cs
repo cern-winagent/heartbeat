8e552ed3139df0edfd28c437ad6086975b277626
Esteban Puentes <esteban.puentes@cern.ch>
2019-08-12 16:17:18 +0200

    Add Operating System information

--- heartbeat/IHeartbeat.cs
+++ heartbeat/IHeartbeat.cs
@@ -1,24 +1,60 @@
 ﻿using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System;
+using System.Management;
+using System.Linq;
 
 using plugin;
+using heartbeat.Models;
 
 namespace heartbeat
 {
     [PluginAttribute(PluginName = "HeartBeat")]
     public class IHeartbeat : IInputPlugin
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns>void</returns>
         public string Execute(JObject settings)
         {
-            var heartBeatMessage = new Models.HeartBeatMessage
+            var OSInfo = GetOperativeSystem();
+
+            return JsonConvert.SerializeObject(OSInfo);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="System.Management.ManagementException: Not found">Thrown when one of the fields could not be found in the result of the query</exception>
+        /// <exception cref="System.Management.ManagementException: Invalid class">Thrown when the class specified in the query is invalid</exception>
+        /// <exception cref="System.Management.ManagementException: Invalid query">Thrown when there is an error in the syntax of the query, including an invalid field</exception>
+        /// <returns>OperatingSystem</returns>
+        private Models.OperativeSystemInfo GetOperativeSystem()
+        {
+            try
             {
-                HostName = System.Environment.MachineName,
-                OperativeSystem = System.Environment.OSVersion.VersionString,
-                DateTime = DateTime.Now
-            };
-            
-            return JsonConvert.SerializeObject(heartBeatMessage);
+                //Query system for Operating System information
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CSName, BuildNumber, Caption, Version, LastBootUpTime, LocalDateTime FROM Win32_OperatingSystem");
+                ManagementObject OS = searcher.Get().OfType<ManagementObject>().FirstOrDefault();
+
+                var operativeSystem = new Models.OperativeSystemInfo()
+                {
+                    HostName = OS["CSName"].ToString(),
+                    BuildNumber = OS["BuildNumber"].ToString(),
+                    OperatingSystem = OS["Caption"].ToString(),
+                    Version = OS["Version"].ToString(),
+                    LastBootUpTime = ManagementDateTimeConverter.ToDateTime(OS["LastBootUpTime"].ToString()),
+                    LocalDateTime = ManagementDateTimeConverter.ToDateTime(OS["LocalDateTime"].ToString())
+                };
+
+                return operativeSystem;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
     }
---
+++ heartbeat/Models/OperativeSystemInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heartbeat.Models
+{
+    class OperativeSystemInfo
+    {
+        public string HostName { set; get; }
+
+        public string OperatingSystem { get; set; }
+
+        public string Version { get; set; }
+
+        public string BuildNumber { get; set; }
+
+        public DateTime LastBootUpTime { get; set; }
+
+        public DateTime LocalDateTime { get; set; }
+    }
+}
